c488f987844d92a2e7df3ce26c201a476346f249
Robert Vo <robertvo79@gmail.com>
2016-02-27 16:21:12 -0600

    Created database_schema.sql

---
+++ database_schema.sql
@@ -0,0 +1,37 @@
+drop table if exists stores;
+DROP TABLE if exists employees;
+
+CREATE TABLE employees (
+	emp_id INT auto_increment primary key, 
+    first_name VARCHAR(20), 
+    last_name VARCHAR(30)
+);
+
+create table stores (
+	storeID INT auto_increment primary key,
+    store_name VARCHAR(30),
+    store_location VARCHAR(30),
+    manager_id int,
+    regional_manager_id int,
+    foreign key (manager_id) references employees(emp_id),
+    foreign key (regional_manager_id) references employees(emp_id)
+);
+
+INSERT INTO employees (first_name, last_name) VALUES ('John', 'Doe');
+INSERT INTO employees (first_name, last_name) VALUES ('Bob', 'Smith');
+INSERT INTO employees (first_name, last_name) VALUES ('Jane', 'Doe');
+
+INSERT INTO stores (store_name, 
+					store_location, 
+                    manager_id, 
+                    regional_manager_id) 
+	   VALUES ('McDonalds', 'Houston, TX', 2, 1);
+       
+INSERT INTO stores (store_name, 
+					store_location, 
+                    manager_id, 
+                    regional_manager_id) 
+	   VALUES ('McDonalds', 'Austin, TX', 3, 1);
+
+SELECT * FROM employees;
+select * from stores;
