72543c0772035b830f7b544683d55bf4602c8e35
Robert Vo <robertvo79@gmail.com>
2016-02-27 16:25:39 -0600

    Added an insert that should fail.
    The last insert statement should fail because an employee_id of 4 does not exist, thus violating the foreign key constraint.

--- database_schema.sql
+++ database_schema.sql
@@ -1,14 +1,15 @@
 drop table if exists stores;
+
 DROP TABLE if exists employees;
 
 CREATE TABLE employees (
-	emp_id INT auto_increment primary key, 
+    emp_id INT auto_increment primary key, 
     first_name VARCHAR(20), 
     last_name VARCHAR(30)
 );
 
 create table stores (
-	storeID INT auto_increment primary key,
+    storeID INT auto_increment primary key,
     store_name VARCHAR(30),
     store_location VARCHAR(30),
     manager_id int,
@@ -23,8 +24,8 @@ INSERT INTO employees (first_name, last_name) VALUES ('Jane', 'Doe');
 
 INSERT INTO stores (store_name, 
 		store_location, 
-                manager_id, 
-                regional_manager_id) 
+        	manager_id, 
+        	regional_manager_id) 
 	VALUES ('McDonalds', 'Houston, TX', 2, 1);
        
 INSERT INTO stores (store_name, 
@@ -33,5 +34,12 @@ INSERT INTO stores (store_name,
                 regional_manager_id) 
 	VALUES ('McDonalds', 'Austin, TX', 3, 1);
 
+-- should fail since an employee id of 4 does not exist. 
+INSERT INTO stores (store_name, 
+		store_location, 
+                manager_id, 
+                regional_manager_id) 
+	VALUES ('McDonalds', 'Austin, TX', 4, 1);
+
 SELECT * FROM employees;
-select * from stores;
+select * from stores; --displays only two rows.
