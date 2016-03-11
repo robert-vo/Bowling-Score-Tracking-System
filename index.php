591b847f0a0a3fccb855cdefc3dd931506700a9e
Robert Vo <robertvo79@gmail.com>
2016-03-10 23:24:36 -0600

    Initial index.php file with a bootstrap example.

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+<?php
+
+function printAllFromBalls() {
+    $servername = "localhost:3306";
+    $username = "root";
+    $password = "password";
+    $dbname = "bowling";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM Ball";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<br> Color: ". $row["Color"]. " - Weight: ". $row["Weight"]. " - Size: " . $row["Size"] . "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+
+    $conn->close();
+}
+?>
+
+
+
+<div class="container">
+    <div class="jumbotron">
+        <h1>My First Bootstrap Page</h1>
+        <p>Resize this responsive page to see the effect!</p>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
+            <h3>Balls</h3>
+            <p><?php printAllFromBalls()?></p>
+        </div>
+        <div class="col-sm-4">
+            <h3>Column 2</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        </div>
+        <div class="col-sm-4">
+            <h3>Column 3</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        </div>
+    </div>
+</div>
+
+
+</body>
+
+
+</html>
