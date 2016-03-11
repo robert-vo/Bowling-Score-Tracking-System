4653bf4cb523c5927a8545fe6b2250ea42d3b7f7
robert-vo <robertvo79@gmail.com>
2016-03-10 23:31:27 -0600

    Added initial values for Ball table.

--- insert_data.sql
+++ insert_data.sql
@@ -1,1 +1,15 @@
 use bowling;
+
+
+insert into Ball(Color, Weight, Size)
+	Values('Blue', 10, 'X');
+    
+insert into Ball(Color, Weight, Size)
+	Values('Red', 05, 'S');
+    
+insert into Ball(Color, Weight, Size)
+	Values('Purple', 08, 'M');
+    
+insert into Ball(Color, Weight, Size)
+	Values('Pink', 15, 'XL');
+    
