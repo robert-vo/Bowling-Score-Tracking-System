1aa002600eb452bf5eabe46cbe4305fd63b97555
robert-vo <robertvo79@gmail.com>
2016-03-21 15:03:49 -0500

    General formatting.
    Changed frame/roll foreign key relationship.
    
    Added defaults to some attributes.

--- resources/database_schema.sql
+++ resources/database_schema.sql
@@ -1,7 +1,7 @@
 -- https://docs.google.com/document/d/1SkxY0NnXMzxtM450nEUy3GQ_7iAWJgRgjFCNVYfvdiE/edit
 
 /*
-create table format
+CREATE TABLE format
 CREATE TABLE table_name (
   attribute_name  data_type  any_other_special_properties,
   ...,
@@ -15,125 +15,142 @@ CREATE DATABASE if not exists bowling;
 
 use bowling;
 
-drop table if exists Game;
-drop table if exists Events;
-drop table if exists Event_Types;
-drop table if exists Statistics;
-drop table if exists Frame;
-drop table if exists Roll;
-drop table if exists Team;
-drop table if exists Ball;
-drop table if exists Players;
+DROP TABLE IF EXISTS Game;
+DROP TABLE IF EXISTS Events;
+DROP TABLE IF EXISTS Event_Types;
+DROP TABLE IF EXISTS Statistics;
+DROP TABLE IF EXISTS Frame;
+DROP TABLE IF EXISTS Roll;
+DROP TABLE IF EXISTS Team;
+DROP TABLE IF EXISTS Ball;
+DROP TABLE IF EXISTS Players;
 
-create table Ball (
-  Ball_ID  int          primary key AUTO_INCREMENT,
-  Color    varchar(20)  not null,
-  Weight   int          not NULL,
-  Size     ENUM('XS', 'S', 'M', 'L', 'XL', 'XXL')
+CREATE TABLE Ball (
+  Ball_ID  INT          primary key AUTO_INCREMENT,
+  Color    VARCHAR(20)  NOT NULL,
+  Weight   INT          not NULL,
+  Size     ENUM('XS', 'S', 'M', 'L', 'XL', 'XXL') NOT NULL
 );
 
-create table Players (
-  Player_ID       int           primary key AUTO_INCREMENT,
-  Gender          ENUM('F', 'M'),
-  Phone_Number    varchar(15),
-  Date_Joined     date          not null,
-  Date_Of_Birth   datetime      not null,
-  Address         varchar(30),
-  First_Name      varchar(30)   not null,
-  Last_Name       varchar(30)   not null,
-  Middle_Initial  varchar(1),
-  Email           varchar(30)   not null
+CREATE TABLE Players (
+  Player_ID       INT             primary key AUTO_INCREMENT,
+  Gender          ENUM('F', 'M')  NOT NULL,
+  PhONe_Number    VARCHAR(15),
+  Date_Joined     DATE            NOT NULL,
+  Date_Of_Birth   DATETIME        NOT NULL,
+  Address         VARCHAR(30),
+  First_Name      VARCHAR(30)     NOT NULL,
+  Last_Name       VARCHAR(30)     NOT NULL,
+  Middle_Initial  VARCHAR(1),
+  Email           VARCHAR(30)     NOT NULL
 );
 
-create table Team (
-  Team_ID        int            primary key AUTO_INCREMENT,
-  Name           varchar(20),
-  Leader         int,
-  Date_Created   datetime,
-  Game_Count     int,
-  Win_Count      int,
-  Phone_Number   varchar(15),
-  Address        varchar(30),
-  Player_1	     int,
-  Player_2	     int            null,
-  Player_3	     int            null,
-  Player_4	     int            null,
-  Player_5	     int            null,
-  foreign key    (Leader)       references Players(Player_ID),
-  foreign key    (Player_1)     references Players(Player_ID),
-  foreign key    (Player_2)     references Players(Player_ID),
-  foreign key    (Player_3)     references Players(Player_ID),
-  foreign key    (Player_4)     references Players(Player_ID),
-  foreign key    (Player_5)     references Players(Player_ID)
+CREATE TABLE Team (
+  Team_ID        INT            primary key AUTO_INCREMENT,
+  Name           VARCHAR(20)	  NOT NULL,
+  Leader         INT			      NOT NULL,
+  Date_Created   DATETIME		    NOT NULL,
+  Game_Count     INT			      default 0,
+  Win_Count      INT			      default 0,
+  PhONe_Number   VARCHAR(15),
+  Address        VARCHAR(30),
+  Player_1	     INT,
+  Player_2	     INT            null,
+  Player_3	     INT            null,
+  Player_4	     INT            null,
+  Player_5	     INT            null,
+  FOREIGN KEY    (Leader)       REFERENCES Players(Player_ID),
+  FOREIGN KEY    (Player_1)     REFERENCES Players(Player_ID),
+  FOREIGN KEY    (Player_2)     REFERENCES Players(Player_ID),
+  FOREIGN KEY    (Player_3)     REFERENCES Players(Player_ID),
+  FOREIGN KEY    (Player_4)     REFERENCES Players(Player_ID),
+  FOREIGN KEY    (Player_5)     REFERENCES Players(Player_ID)
 );
 
-create table Roll (
-  Roll_ID     int     primary key AUTO_INCREMENT,
-  Frame_ID    int     not null,
-  Ball_ID     int     not null,
-  Is_Strike   boolean not null,
-  Is_Spare    boolean not null,
-  Is_Foul     boolean not null,
-  Hit_Pin_1		boolean not null,
-  Hit_Pin_2		boolean not null,
-  Hit_Pin_3		boolean not null,
-  Hit_Pin_4		boolean not null,
-  Hit_Pin_5		boolean not null,
-  Hit_Pin_6		boolean not null,
-  Hit_Pin_7		boolean not null,
-  Hit_Pin_8		boolean not null,
-  Hit_Pin_9		boolean not null,
-  Hit_Pin_10	boolean not null,
-  foreign key (Ball_ID) REFERENCES Ball(Ball_ID) on delete CASCADE
+CREATE TABLE Frame(
+  Frame_ID        INT primary key AUTO_INCREMENT,
+  Player_ID       INT,
+  Roll_One_ID     INT,
+  Roll_Two_ID     INT,
+  Roll_Three_ID   INT,
+  Score           INT DEFAULT 0,
+  FOREIGN KEY (Player_ID) REFERENCES Players(Player_ID)
 );
 
-create table Frame(
-  Frame_ID        int primary key AUTO_INCREMENT,
-  Player_ID       int,
-  Roll_One_ID     int,
-  Roll_Two_ID     int,
-  Roll_Three_ID   int null,
-  Score           int,
-  foreign key (Player_ID) references Players(Player_ID),
-  foreign key (Roll_One_ID) references Roll(Roll_ID),
-  foreign key (Roll_Two_ID) references Roll(Roll_ID),
-  foreign key (Roll_Three_ID) references Roll(Roll_ID)
+CREATE TABLE Roll (
+  Roll_ID     INT     primary key AUTO_INCREMENT,
+  Frame_ID    INT     NOT NULL,
+  Ball_ID     INT,
+  Is_Strike   BOOLEAN NOT NULL DEFAULT FALSE,
+  Is_Spare    BOOLEAN NOT NULL DEFAULT FALSE,
+  Is_Foul     BOOLEAN NOT NULL DEFAULT FALSE,
+  Hit_Pin_1		BOOLEAN NOT NULL DEFAULT FALSE,
+  Hit_Pin_2		BOOLEAN NOT NULL DEFAULT FALSE,
+  Hit_Pin_3		BOOLEAN NOT NULL DEFAULT FALSE,
+  Hit_Pin_4		BOOLEAN NOT NULL DEFAULT FALSE,
+  Hit_Pin_5		BOOLEAN NOT NULL DEFAULT FALSE,
+  Hit_Pin_6		BOOLEAN NOT NULL DEFAULT FALSE,
+  Hit_Pin_7		BOOLEAN NOT NULL DEFAULT FALSE,
+  Hit_Pin_8		BOOLEAN NOT NULL DEFAULT FALSE,
+  Hit_Pin_9		BOOLEAN NOT NULL DEFAULT FALSE,
+  Hit_Pin_10	BOOLEAN NOT NULL DEFAULT FALSE,
+  FOREIGN KEY (Ball_ID) REFERENCES Ball(Ball_ID)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  FOREIGN KEY (Frame_ID) REFERENCES Frame(Frame_ID)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
-create table Events (
-  Event_ID    int primary key AUTO_INCREMENT,
-  Game_ID     int,
-  Team_ID     int,
-  Event_Time  datetime,
-  Winner      varchar(20),
-  Title       varchar(20),
-  Location    varchar(50),
+CREATE TABLE Events (
+  Event_ID    INT primary key AUTO_INCREMENT,
+  Game_ID     INT,
+  Team_ID     INT,
+  Event_Time  DATETIME,
+  Winner      VARCHAR(20),
+  Title       VARCHAR(20),
+  LocatiON    VARCHAR(50),
   Event_Type  ENUM('Casual', 'Tournament'),
-  foreign key (Team_ID) references Team(Team_ID)
+  FOREIGN KEY (Team_ID) REFERENCES Team(Team_ID)
 );
 
-create table Game (
-  Game_ID     int  primary key AUTO_INCREMENT,
-  Event_ID    int,
-  Team_ID     int,
-  Frame_ID    int,
-  foreign key (Event_ID) references Events(Event_ID),
-  foreign key (Team_ID) references Team(Team_ID),
-  foreign key (Frame_ID) references Frame(Frame_ID)
+CREATE TABLE Game (
+  Game_ID     INT  primary key AUTO_INCREMENT,
+  Event_ID    INT,
+  Team_ID     INT,
+  Frame_ID    INT,
+  FOREIGN KEY (Event_ID) REFERENCES Events(Event_ID),
+  FOREIGN KEY (Team_ID) REFERENCES Team(Team_ID),
+  FOREIGN KEY (Frame_ID) REFERENCES Frame(Frame_ID)
 );
 
-create table Statistics (
-  Stat_ID           int  primary key AUTO_INCREMENT,
-  Player_ID         int,
-  Strikes           int,
-  Perfect_Games     int,
-  Spares            int,
-  Best_Score        int,
-  Worst_Score       int,
-  Pins_Left         int,
-  Average_Pin_Left  int,
-  foreign key (Player_ID) references Players(Player_ID)
-    on delete CASCADE
-    on update CASCADE
+CREATE TABLE Statistics (
+  Stat_ID           INT primary key AUTO_INCREMENT,
+  Player_ID         INT,
+  Strikes           INT NOT NULL default 0,
+  Perfect_Games     INT NOT NULL default 0,
+  Spares            INT NOT NULL default 0,
+  Best_Score        INT NOT NULL default 0,
+  Worst_Score       INT NOT NULL default 0,
+  Pins_Left         INT NOT NULL default 0,
+  Average_Pin_Left  INT NOT NULL default 0,
+  FOREIGN KEY (Player_ID) REFERENCES Players(Player_ID)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
+INSERT INTO Ball(Color, Weight, Size)
+Values('hello world', 10, 'XS');
+
+INSERT INTO Ball(Color, Weight, Size)
+Values('this is cONnecting', 05, 'S');
+
+INSERT INTO Ball(Color, Weight, Size)
+Values('to an', 08, 'M');
+
+INSERT INTO Ball(Color, Weight, Size)
+Values('azure database', 15, 'XL');
+
+INSERT INTO Ball(Color, Weight, Size)
+Values('azure database', 15, 1);
+select * from ball;
