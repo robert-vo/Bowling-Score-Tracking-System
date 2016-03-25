f68e7a353913a8bbe3d2fac7d05c10f64d2564bf
Robert Vo <robertvo79@gmail.com>
2016-03-25 12:12:43 -0500

    Capitalized Ball.

--- resources/insert_data.sql
+++ resources/insert_data.sql
@@ -1,6 +1,6 @@
 use bowling;
-INSERT INTO bowling.ball (Color, Weight, Size) VALUES ('Blue', 10, 'M');
-INSERT INTO bowling.ball (Color, Weight, Size) VALUES ('Red', 5, 'S');
-INSERT INTO bowling.ball (Color, Weight, Size) VALUES ('Purple', 8, 'M');
-INSERT INTO bowling.ball (Color, Weight, Size) VALUES ('Pink', 15, 'XL');
-INSERT INTO bowling.ball (Color, Weight, Size) VALUES ('Blue', 14, 'm');
+INSERT INTO bowling.Ball (Color, Weight, Size) VALUES ('Blue', 10, 'M');
+INSERT INTO bowling.Ball (Color, Weight, Size) VALUES ('Red', 5, 'S');
+INSERT INTO bowling.Ball (Color, Weight, Size) VALUES ('Purple', 8, 'M');
+INSERT INTO bowling.Ball (Color, Weight, Size) VALUES ('Pink', 15, 'XL');
+INSERT INTO bowling.Ball (Color, Weight, Size) VALUES ('Blue', 14, 'm');
