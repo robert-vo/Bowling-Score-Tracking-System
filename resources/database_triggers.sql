a57ba55747703a7966839b711a8248de9d4fa525
Robert Vo <robertvo79@gmail.com>
2016-03-27 18:31:39 -0500

    Create database_triggers.sql

---
+++ resources/database_triggers.sql
@@ -0,0 +1,7 @@
+-- This file contains all SQL code that will create the triggers in the bowling database. 
+
+-- This trigger automatically creates the date for when the player joins.
+drop trigger if exists players_date_joined;
+CREATE TRIGGER players_date_joined BEFORE INSERT ON Players
+FOR EACH ROW
+  SET NEW.Date_Joined = NOW();
