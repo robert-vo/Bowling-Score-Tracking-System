d72eb471f1cd974580cd7fe3014ca240d7a2cb7f
robert-vo <robertvo79@gmail.com>
2016-03-28 17:52:16 -0500

    Inserted the first player.

--- resources/insert_data.sql
+++ resources/insert_data.sql
@@ -20,3 +20,6 @@ INSERT INTO bowling.Ball (Color, Weight, Size) VALUES ('White', 12, 'XL');
 INSERT INTO bowling.Ball (Color, Weight, Size) VALUES ('Pink', 5, 'S');
 INSERT INTO bowling.Ball (Color, Weight, Size) VALUES ('Polka Dot', 12, 'M');
 INSERT INTO bowling.Ball (Color, Weight, Size) VALUES ('Dark Blue', 12, 'XL');
+
+
+INSERT INTO bowling.Players (Gender, Phone_Number, Date_Joined, Date_Of_Birth, Street_Address, City, State, Zip_Code, First_Name, Last_Name, Middle_Initial, Email, Password) VALUES ('M', '123-123-1234', null, '1994-03-02', '1234 Main Street', 'Houston', 'TX', 77002, 'Macy', 'Smith', null, 'macy@smith.com', '$2y$10$GsLtBLZMXhihUhcRTo0qyu.lXYH4efw9qEO/HHvzKJIYdy7/JJv9i');
