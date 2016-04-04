12f12979028eabc1b66c3318b44e6010dcfd7ca2
William <wnguyen23@gmail.com>
2016-04-04 18:40:23 -0500

    -added missing files

---
+++ public_html/settings.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title> Settings </title>
+
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="stylesheet" type="text/css" href="loginAndRegistrationForm.css">
+    <link rel="stylesheet" type="text/css" href="settings.css">
+
+    <style>
+        div#error{
+            color: red;
+        }
+    </style>
+</head>
+
+<body>
+
+
+<?php include 'menuBar.php';
+generateMenuBar(basename(__FILE__));
+
+?>
