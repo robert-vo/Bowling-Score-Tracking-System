3245dfba57121b6420d09f0e4ecf6c3fcffedf05
William <wnguyen23@gmail.com>
2016-04-04 00:05:27 -0500

    -Added Team, Bowling_Events, Frame, Roll, Game, Player_Stats Triggers

--- resources/database_triggers.sql
+++ resources/database_triggers.sql
@@ -32,3 +32,34 @@ CREATE TRIGGER delete_from_player AFTER DELETE ON Players
   FOR EACH ROW
   insert into Players_Archive VALUES (old.Player_ID, old.Gender, old.Phone_Number, old.Date_Joined, old.Date_of_Birth, old.Street_Address ,old.City, old.State, old.Zip_Code, old.First_Name, old.Last_Name, old.Middle_Initial, old.Email, old.Password, old.Is_Admin, now());
 
+drop trigger if exists delete_from_teams;
+CREATE TRIGGER delete_from_teams AFTER DELETE ON Team
+  FOR EACH ROW
+  insert into Team_Archive VALUES (old.TEAM_ID, old.Name, old.Leader, old.Date_Created, old.Game_Count, old.Win_Count, old.Player_1, old.Player_2, old.Player_3, old.Player_4, old.Player_5);
+
+drop trigger if exists delete_from_bowlingEvents;
+CREATE TRIGGER delete_from_bowlingEvents AFTER DELETE ON Bowling_Events
+FOR EACH ROW
+  insert into Bowling_Events_Archive VALUES (old.Event_ID, old.Team_ID, old.Event_Time, old.Winner, old.Title, old.Location, old.Event_Type);
+
+drop trigger if exists delete_from_Frame;
+CREATE TRIGGER delete_from_Frame AFTER DELETE ON Frame
+FOR EACH ROW
+  insert into Frame_Archive VALUES (old.Frame_ID, old.Frame_Number, old.Player_ID, old.Roll_One_ID, old.Roll_Two_ID, old.Roll_Three_ID, old.Score, old.Team_ID, old.Event_ID);
+
+drop trigger if exists delete_from_Roll;
+CREATE TRIGGER delete_from_Roll AFTER DELETE ON Roll
+FOR EACH ROW
+  insert into Roll_Archive VALUES (old.Roll_ID, old.Frame_ID, old.Ball_ID);
+
+drop trigger if exists delete_from_Game;
+CREATE TRIGGER delete_from_Game AFTER DELETE ON Game
+FOR EACH ROW
+  insert into Game_Archive VALUES (old.Game_ID, old.Event_ID, old.Teams);
+
+drop trigger if exists delete_from_PlayerStats;
+CREATE TRIGGER delete_from_PlayerStats AFTER DELETE ON Player_Stats
+FOR EACH ROW
+  insert into Player_Stats_Archive VALUES (old.Stat_ID, old.Player_ID, old.Strikes, old.Games_Played, old.Perfect_Games, old.Spares, old.Best_Score, old.Worst_Score, old.Pins_Left, old.Average_Pin_Left);
+
+
