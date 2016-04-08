5ec0bc47851e8e360758296ee8fea8c862427413
robert-vo <robertvo79@gmail.com>
2016-04-08 15:48:11 -0500

    Changed the static menu bar on audit to the dynamic version.

--- public_html/audit.php
+++ public_html/audit.php
@@ -11,13 +11,9 @@
 <body>
 
 
-<ul>
-    <li><a href="index.php">Home</a></li>
-    <li><a href="scores.php">Scores</a></li>
-    <li style="float:right"><a class="active" href="audit.php">Audit</a></li>
-    <li style="float:right"><a href="about.php">About</a></li>
-    <li style="float:right"><a href="loginF.php">Login</a></li>
-</ul>
+<?php include 'menuBar.php';
+generateMenuBar(basename(__FILE__));
+?>
 
 <p>
     Which table do you want to edit?
