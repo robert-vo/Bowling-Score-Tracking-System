6555d5515b51bc780f82bce89dc9f5b3a02ce282
robert-vo <robertvo79@gmail.com>
2016-04-08 18:46:04 -0500

    Changed variables back to normal.

--- public_html/databaseFunctions.php
+++ public_html/databaseFunctions.php
@@ -5,23 +5,17 @@ $typeOfConnection = getenv('typeOfConnection');
 
 function connectToDatabase()
 {
-    echo 'in function';
-    echo getenv('databaseUsername');
-    echo getenv('databaseConnection');
-    echo getenv('databasePassword');
-    echo getenv('databaseName');
-    echo getenv('typeOfConnection');
-//    if ($GLOBALS['typeOfConnection'] == 'Remote') {
-        $servername = getenv('databaseConnection');
-        $username = getenv('databaseUsername');
-        $password = getenv('databasePassword');
-        $dbname = getenv('databaseName');
-//    } else {
-//        $servername = "localhost:3306";
-//        $username = "root";
-//        $password = "password";
-//        $dbname = "bowling";
-//    }
+    if ($GLOBALS['typeOfConnection'] == 'Remote') {
+        $servername = "mydbinstance2.cnotb9fanxgq.us-west-2.rds.amazonaws.com:3306/bowling";
+        $username = "bowlingdb";
+        $password = "bowlingdb";
+        $dbname = "bowling";
+    } else {
+        $servername = "localhost:3306";
+        $username = "root";
+        $password = "password";
+        $dbname = "bowling";
+    }
     return new mysqli($servername, $username, $password, $dbname);
 }
 
