0bc3f7219c7afa6e219c48cf7f67688297a94bb0
robert-vo <robertvo79@gmail.com>
2016-04-09 02:29:56 -0500

    Generalized the dropdown box for all columns in the bowling database.

--- public_html/audit.php
+++ public_html/audit.php
@@ -20,14 +20,18 @@ generateMenuBar(basename(__FILE__));
     <form action="runAudit.php" method="post">
     <select name="bowlingAudit">
         <option value="">Select...</option>
-        <option value="Ball">Ball</option>
-        <option value="Bowling_Events">Bowling Events</option>
-        <option value="Frame">Frame</option>
-        <option value="Game">Game</option>
-        <option value="Players">Players</option>
-        <option value="Roll">Roll</option>
-        <option value="Player_Stats">Player Stats</option>
-        <option value="Team">Team</option>
+        <?php
+        include 'databaseFunctions.php';
+
+        $connection = connectToDatabase();
+
+        $result = $connection->query("select table_name from information_schema.tables where table_schema = 'bowling';");
+        $rows = [];
+        while($row = $result->fetch_assoc())
+        {
+            echo "<option value =\"" . $row["table_name"] . "\">" . $row["table_name"] . "</option>";
+        }
+        ?>
     </select>
     <input type="submit" value="Submit">
 </form>
