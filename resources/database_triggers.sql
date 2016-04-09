a8e4ef00e92972fceb2f481c31a340165458e762
robert-vo <robertvo79@gmail.com>
2016-04-08 19:48:51 -0500

    Experimenting with delimiter

--- resources/database_triggers.sql
+++ resources/database_triggers.sql
@@ -27,22 +27,24 @@ FOR EACH ROW
   SET NEW.Game_Start_Time = NOW();
 
 drop trigger if exists game_finished;
+delimiter $$
 CREATE TRIGGER game_finished before UPDATE ON Game
 FOR EACH ROW
   BEGIN
     if (new.Game_Finished = true) then
         set new.Game_End_Time = now();
     end IF;
-  END;
+  END$$
 
 drop trigger if exists game_finished;
+delimiter $$
 CREATE TRIGGER game_finished before insert ON Game
 FOR EACH ROW
   BEGIN
     if (new.Game_Finished = true) then
       set new.Game_End_Time = now();
     end IF;
-  END;
+  END$$
 
 -- Other triggers for archiving other tables
 drop trigger if exists delete_from_ball;
