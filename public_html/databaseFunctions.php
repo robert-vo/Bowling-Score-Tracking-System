7d80996563cc74a8aa2c4b200e81a98e77a14fb2
robert-vo <robertvo79@gmail.com>
2016-04-10 20:29:30 -0500

    Added a function to get all teams that a player is a part of (leader or not).

--- public_html/databaseFunctions.php
+++ public_html/databaseFunctions.php
@@ -11,12 +11,29 @@ function connectToDatabase()
         $password = "bowlingdb";
         $dbname = "bowling";
     } else {
-        $servername = "localhost:3307";
+        $servername = "localhost:3306";
         $username = "root";
         $password = "password";
         $dbname = "bowling";
     }
     return new mysqli($servername, $username, $password, $dbname);
+}
+
+function returnResultForQuery($query) {
+    $connection = connectToDatabase();
+    return $connection->query($query);
+}
+
+function getAllTeamsForAPlayerID($playerID) {
+    $query = "SELECT * FROM team 
+              WHERE Leader = '$playerID' 
+                OR Player_1 = '$playerID' 
+                OR Player_2 = '$playerID' 
+                OR Player_3 = '$playerID' 
+                OR Player_4 = '$playerID' 
+                OR Player_5 = '$playerID'";
+
+    return returnResultForQuery($query);
 }
 
 ?>
