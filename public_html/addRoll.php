e880bddadfe4cfed6a89924a8a8fc0192756e230
robert-vo <robertvo79@gmail.com>
2016-04-16 00:52:17 -0500

    WIP: Got all 10 pins to show up.

--- public_html/addRoll.php
+++ public_html/addRoll.php
@@ -4,11 +4,15 @@ include 'menuBar.php';
 generateMenuBar(basename(__FILE__));
 include 'databaseFunctions.php';
 
-if(isset($_POST['pin1'])) {
-    echo 'yay pin 1 is set';
-}
-else {
-    echo 'not yet :(';
+if(isset($_POST['submit'])) {
+    if(!isset($_POST['pins'])) {
+        echo 'no pins hit!';
+    }
+    else {
+        foreach($_POST['pins'] as $pin) {
+            echo 'You have hit pin ' . $pin . '<br>';
+        }
+    }
 }
 ?>
 
@@ -21,19 +25,20 @@ else {
 <body>
 
 <p>
-<form action="addRoll.php">
-
-    <input type='checkbox' name='pin1' value='yes'/><label></label>
-    <!--        <input type='checkbox' name='pin2' value='pin2'/><label></label>-->
-    <!--        <input type='checkbox' name='pin3' value='pin3'/><label></label>-->
-    <!--        <input type='checkbox' name='pin4' value='pin4'/><label></label>-->
-    <!--        <input type='checkbox' name='pin5' value='pin5'/><label></label>-->
-    <!--        <input type='checkbox' name='pin6' value='pin6'/><label></label>-->
-    <!--        <input type='checkbox' name='pin7' value='pin7'/><label></label>-->
-    <!--        <input type='checkbox' name='pin8' value='pin8'/><label></label>-->
-    <!--        <input type='checkbox' name='pin9' value='pin9'/><label></label>-->
-    <!--        <input type='checkbox' name='pin10' value='pin10'/><label></label>-->
-
+<form action="addRoll.php" method="POST">
+    <div class = "pin1">
+        <input class = "bottomright" type='checkbox' name='pins[]' value='1' id = 'pin1'/><label for ='pin1'>1</label>
+    </div>
+    <input type='checkbox' name='pins[]' value='2' id = 'pin2'/><label for ='pin2'>2</label>
+    <input type='checkbox' name='pins[]' value='3' id = 'pin3'/><label for ='pin3'>3</label>
+    <input type='checkbox' name='pins[]' value='4' id = 'pin4'/><label for ='pin4'>4</label>
+    <input type='checkbox' name='pins[]' value='5' id = 'pin5'/><label for ='pin5'>5</label>
+    <input type='checkbox' name='pins[]' value='6' id = 'pin6'/><label for ='pin6'>6</label>
+    <input type='checkbox' name='pins[]' value='7' id = 'pin7'/><label for ='pin7'>7</label>
+    <input type='checkbox' name='pins[]' value='8' id = 'pin8'/><label for ='pin8'>8</label>
+    <input type='checkbox' name='pins[]' value='9' id = 'pin9'/><label for ='pin9'>9</label>
+    <input type='checkbox' name='pins[]' value='10' id = 'pin10'/><label for ='pin10'>10</label>
+    <br>
     <input type="submit" value="Submit Roll" name="submit">
 </form>
 </body>
