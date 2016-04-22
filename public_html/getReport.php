71d822b9c41959ef7a752ba849cf39d9bf6a5ba1
mkpham <mkpham3@uh.edu>
2016-04-22 12:51:24 -0500

    Added categories for the reports  and some options for the player reports

---
+++ public_html/getReport.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Scores</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <style>
+        table#report{
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        tr:nth-child(even) {
+            background-color: #e6e6e6;
+        }
+
+        tr.table_header {
+            background-color: #4CAF50;
+            color: #FFFFFF;
+        }
+    </style>
+</head>
+
+<?php
+include 'databaseFunctions.php';
+
+
+function createPlayersArray() {
+    $conn = connectToDatabase();
+    $query = "SELECT First_Name, Last_Name, Player_ID from Players";
+    $result = $conn->query($query);
+    $teams = array();
+    if($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $teams[$row['Player_ID']] = $row['First_Name'] . " " . $row['Last_Name'];
+        }
+    } else {
+        echo "0 results";
+    }
+    return $teams;
+}
+
+
+function printTop10Teams($orderBy, $showTop) {
+    $conn = connectToDatabase();
+    $query = "SELECT * FROM Team ORDER BY Win_Count " . $orderBy . " LIMIT " . $showTop;
+    $result = $conn->query($query);
+    printResult($result);
+    $conn->close();
+}
+
+
+function printTop10TeamsByPercentage($orderBy, $showTop) {
+    $conn = connectToDatabase();
+    $query = 'SELECT Name, Leader, Game_Count, Win_Count, 100*Win_Count/Team.Game_Count FROM TEAM ORDER BY 100*Win_Count/Team.Game_Count ' . $orderBy . ' LIMIT ' . $showTop;
+    $result = $conn->query($query);
+    printResult($result);
+    $conn->close();
+}
+
+
+function printTop10TeamsGamesPlayed($orderBy, $showTop) {
+    $conn = connectToDatabase();
+    $query = "SELECT Name, Leader, Game_Count, Win_Count, 100*Win_Count/Team.Game_Count FROM TEAM ORDER BY Game_Count " . $orderBy . " LIMIT " . $showTop;
+    $result = $conn->query($query);
+    printResult($result);
+    $conn->close();
+}
+
+
+function printPlayersReport($report, $showTop, $orderBy) {
+    $conn = connectToDatabase();
+    $query = 'SELECT * FROM Player_Stats, Players where Player_Stats.Player_ID = Players.Player_ID and Player_Stats.' . $report . ' > 0 ORDER BY Player_Stats.' . $report . ' ' . $orderBy . ' LIMIT ' . $showTop . ';';
+    //echo $query;
+    $result = $conn->query($query);
+    printPlayers($report, $result);
+    $conn->close();
+}
+
+
+function printPlayers($report, $result) {
+    if($result->num_rows > 0) {
+        echo '<table id="report">';
+        echo '<tr class="table_header"><th>Place</th><th>Player Name</th><th>Number of ' . $report . '</th><th>Player\'s Email Address</th></tr>';
+        $place = 1;
+        while($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo '<td>' . $place . '</td>';
+            echo '<td>' . $row['First_Name'] . ' ' . $row['Last_Name'] . '</td>';
+            echo '<td>' . $row[$report] . '</td>';
+            echo '<td>' . $row['Email'] . '</td>';
+            echo '</tr>';
+
+            $place++;
+        }
+        echo '</table>';
+    }
+    else {
+        echo 'Fatal error! No team exists.';
+    }
+}
+
+
+function printResult($result) {
+    if($result->num_rows > 0) {
+        echo "<table id='report'>";
+        echo "<tr class='table_header'><th>Place</th><th>Team Name</th><th>Leader Name</th><th>Games Played</th><th>Win Count</th><th>Win Percentage</th></tr>";
+        $place = 1;
+        $teams = createPlayersArray();
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td align='center'>$place</td>";
+            echo "<td align='center'>" . $row['Name'] . "</td>";
+            echo "<td align='center'>" . $teams[$row['Leader']] . "</td>";
+            echo "<td align='center'>" . $row['Game_Count'] . "</td>";
+            echo "<td align='center'>" . $row['Win_Count'] . "</td>";
+
+            $percentage = (100 * $row['Win_Count']/$row['Game_Count']);
+            echo "<td align='center'>" . number_format($percentage, 0, '.', '') . " %</td>";
+            echo "</tr>";
+
+            $place++;
+        }
+        echo "</table>";
+    } else {
+        echo "Fatal error! No teams exist.";
+    }
+}
+
+
+
+$category = $_POST['category'];
+$reportType = $_POST[ $category . 'ReportType'] ;
+$orderBy = $_POST[ $category . 'OrderBy' ];
+$showTop = $_POST[ $category . 'ShowTop' ];
+
+//echo $reportType . " " . $orderBy . " " . $category . " " . $showTop;
+
+if($category == "players") { // Queries and prints for the 'players' category.
+
+    if($reportType == "Best_Score"){
+
+        $order = "";
+        if ($orderBy == "ASC")
+            $order = 'worst';
+        else
+            $order = 'best';
+        echo '<h5>Here are the top ' . $showTop . ' players who have the ' . $order . ' high score</h5>';
+        printPlayersReport($reportType, $showTop, $orderBy);
+
+    } else {
+
+        $order = "";
+        if ($orderBy == "ASC")
+            $order = 'least';
+        else
+            $order = 'most';
+
+        echo '<h5>Here are the top ' . $showTop . ' players who have rolled the ' . $order . ' ' . $reportType . '</h5>';
+        printPlayersReport($reportType, $showTop, $orderBy);
+    }
+
+}
+else if ($category == "teams") { // Queries and prints for the 'teams' category.
+
+    if($reportType == "win_count") {
+
+        $order = "";
+        if($orderBy == "ASC") {
+            $order = 'lowest';
+        }
+        else {
+            $order = 'highest';
+        }
+
+        echo '<h5>Here are the current top ' . $showTop . ' teams with the ' . $order . ' win count</h5>';
+        printTop10Teams($orderBy, $showTop);
+        
+    } 
+    else if ($reportType == "win_percentage") {
+
+        $order = "";
+        if($orderBy == "ASC") {
+            $order = 'lowest';
+        }
+        else {
+            $order = 'highest';
+        }
+
+        echo '<h5>Here are the current top ' . $showTop . ' teams with the ' . $order . ' win percentage</h5>';
+        printTop10TeamsByPercentage($orderBy, $showTop);
+        
+    }
+    else if ($reportType == "games_played") {
+        
+        $order = "";
+        if($orderBy == "ASC") {
+            $order = 'least';
+        }
+        else {
+            $order = 'most';
+        }
+
+        echo '<h5>Here are the current top ' . $showTop . ' teams who have played the ' . $order . ' games</h5>';
+        printTop10TeamsGamesPlayed($orderBy, $showTop);
+    }
+}
+
+?>
+
+
+<body>
+
+</body>
+</html>
---
+++ public_html/reports.php
@@ -0,0 +1,145 @@
+<?php
+session_start();
+include 'menuBar.php';
+include 'databaseFunctions.php';
+if(!isset($_GET['category']))
+    generateMenuBar(basename(__FILE__));
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Reports</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <style>
+        table {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 10px;
+        }
+
+    </style>
+    <script src="jquery.js"></script>
+    <script type="text/javascript" src="submitReport.js"></script>
+</head>
+
+<body>
+
+<div id="reportPage"><!-- div that holds all of the page's content when page is refreshed -->
+<br><br>
+
+<form>
+    Select a category to get reports from: <select onchange="showSelection(this.value)">
+        <option value="">Choose a category..</option>
+        <?php
+        $category = "";
+        if(isset($_GET['category'])) {      // Selects the default option when the page is reloaded.
+            $category = $_GET['category'];
+            if($category == "players") {
+                echo "<option value=\"players\" selected>Players</option>";
+                echo "<option value=\"teams\">Teams</option>";
+            } else if ($category == "teams") {
+                echo "<option value=\"players\">Players</option>";
+                echo "<option value=\"teams\" selected>Teams</option>";
+            }
+        } else {
+            echo "<option value=\"players\">Players</option>";
+            echo "<option value=\"teams\">Teams</option>";
+        }
+        ?>
+    </select>
+</form>
+
+    <?php
+        $displayPlayers = "style=\"display: none;\"";
+        $displayTeams = "style=\"display: none;\"";
+        if(isset($_GET['category'])) {
+            if($_GET['category'] == "players") {
+                $displayPlayers = "";
+            }
+            else if ($_GET['category'] == "teams") {
+                $displayTeams = "";
+            }
+        }
+    ?>
+    
+<br> <div id="empty"></div>
+
+    <table <?php echo $displayTeams; ?>>
+        <tr>
+            <form>
+                <td>Report Type:
+                    <select id="teamsReportType" name="teamsReportType">
+                        <option value="win_count">Win Count</option>
+                        <option value="win_percentage">Win Percentage</option>
+                        <option value="games_played">Games Played</option>
+                    </select>
+                </td>
+
+                <td>Order by:
+                    <select id="teamsOrderBy" name="teamsOrderBy">
+                        <option value="DESC">High to low</option>
+                        <option value="ASC">Low to high</option>
+                    </select>
+                </td>
+
+                <td>Show top:
+                    <select id="teamsShowTop" name="teamsShowTop">
+                        <option value="5">5</option>
+                        <option value="10">10</option>
+                        <option value="25">25</option>
+                        <option value="50">50</option>
+                        <option value="100">100</option>
+                    </select>
+                </td>
+                <input type="hidden" id="category" value="<?php echo $category;?>">
+                <td><input id="submit_teams" type="submit" value="<?php echo $category;?>"></td>
+
+            </form>
+        </tr>
+    </table>
+
+    <table <?php echo $displayPlayers; ?>>
+        <tr>
+            <form>
+                <td>Report Type:
+                    <select id="playersReportType" name="playersReportType">
+                        <option value="Strikes">Strikes</option>
+                        <option value="Spares">Spares</option>
+                        <option value="Best_Score">Best Score</option>
+                        <option value="Perfect_Games">Perfect Games</option>
+                    </select>
+                </td>
+
+                <td>Order by:
+                    <select id="playersOrderBy" name="playersOrderBy">
+                        <option value="DESC">High to low</option>
+                        <option value="ASC">Low to high</option>
+                    </select>
+                </td>
+
+                <td>Show top:
+                    <select id="playersShowTop" name="playersShowTop">
+                        <option value="5">5</option>
+                        <option value="10">10</option>
+                        <option value="25">25</option>
+                        <option value="50">50</option>
+                        <option value="100">100</option>
+                    </select>
+                </td>
+                <input type="hidden" id="category" value="<?php echo $category;?>">
+                <td><input id="submit_players" type="submit" value="<?php echo $category;?>"></td>
+
+            </form>
+        </tr>
+    </table>
+    <div id="reports"></div>
+
+    
+</div>
+</body>
+</html>
