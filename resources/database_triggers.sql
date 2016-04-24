55388dd23c0346e0cc350aa5ac83058bc4aaffe9
robert-vo <robertvo79@gmail.com>
2016-04-24 18:01:51 -0500

    Created indentions

--- resources/database_triggers.sql
+++ resources/database_triggers.sql
@@ -30,56 +30,86 @@ FOR EACH ROW
 drop trigger if exists delete_from_ball;
 CREATE TRIGGER delete_from_ball AFTER DELETE ON Ball
   FOR EACH ROW
-  insert into Ball_Archive VALUES (old.Ball_ID, old.Color, old.Weight, old.Size, old.Date_Added, old.Last_Date_Modified, now());
+  insert into Ball_Archive VALUES (old.Ball_ID,
+    old.Color, old.Weight, old.Size, old.Date_Added, old.Last_Date_Modified, now());
 
 drop trigger if exists delete_from_player;
 CREATE TRIGGER delete_from_player AFTER DELETE ON Players
   FOR EACH ROW
-  insert into Players_Archive VALUES (old.Player_ID, old.Gender, old.Phone_Number, old.Date_Joined, old.Date_of_Birth, old.Street_Address ,old.City, old.State, old.Zip_Code, old.First_Name, old.Last_Name, old.Middle_Initial, old.Email, old.Password, old.Is_Admin, old.Reset_Key, old.Date_Added, old.Last_Date_Modified, now());
+  insert into Players_Archive VALUES (old.Player_ID,
+    old.Gender, old.Phone_Number, old.Date_Joined, old.Date_of_Birth, old.Street_Address,
+    old.City, old.State, old.Zip_Code, old.First_Name, old.Last_Name, old.Middle_Initial,
+    old.Email, old.Password, old.Is_Admin, old.Reset_Key, old.Date_Added,
+                                      old.Last_Date_Modified, now());
 
 drop trigger if exists delete_from_teams;
 CREATE TRIGGER delete_from_teams AFTER DELETE ON Team
   FOR EACH ROW
-  insert into Team_Archive VALUES (old.TEAM_ID, old.Name, old.Leader, old.Date_Created, old.Game_Count, old.Win_Count, old.Player_1, old.Player_2, old.Player_3, old.Player_4, old.Player_5, old.Date_Added, old.Last_Date_Modified, now());
+  insert into Team_Archive VALUES (old.TEAM_ID, old.Name, old.Leader,
+    old.Date_Created, old.Game_Count, old.Win_Count, old.Player_1,
+    old.Player_2, old.Player_3, old.Player_4, old.Player_5,
+    old.Date_Added, old.Last_Date_Modified, now());
 
 drop trigger if exists delete_from_Frame;
 CREATE TRIGGER delete_from_Frame AFTER DELETE ON Frame
 FOR EACH ROW
-  insert into Frame_Archive VALUES (old.Frame_ID, old.Frame_Number, old.Player_ID, old.Roll_One_ID, old.Roll_Two_ID, old.Roll_Three_ID, old.Score, old.Team_ID, old.Game_ID, old.Date_Added, old.Last_Date_Modified, now());
+  insert into Frame_Archive VALUES (old.Frame_ID, old.Frame_Number,
+    old.Player_ID, old.Roll_One_ID, old.Roll_Two_ID, old.Roll_Three_ID,
+    old.Score, old.Team_ID, old.Game_ID, old.Date_Added, old.Last_Date_Modified, now());
 
 drop trigger if exists delete_from_Roll;
 CREATE TRIGGER delete_from_Roll AFTER DELETE ON Roll
 FOR EACH ROW
-  insert into Roll_Archive VALUES (old.Roll_ID, old.Frame_ID, old.Ball_ID, old.Is_Strike, old.Is_Spare, old.Is_Foul, old.Hit_Pin_1, old.Hit_Pin_2, old.Hit_Pin_3, old.Hit_Pin_4, old.Hit_Pin_5, old.Hit_Pin_6, old.Hit_Pin_7, old.Hit_Pin_8, old.Hit_Pin_9, old.Hit_Pin_10, old.Date_Added, old.Last_Date_Modified, now());
+  insert into Roll_Archive VALUES (old.Roll_ID,
+    old.Frame_ID, old.Ball_ID, old.Is_Strike, old.Is_Spare,
+    old.Is_Foul, old.Hit_Pin_1, old.Hit_Pin_2, old.Hit_Pin_3,
+    old.Hit_Pin_4, old.Hit_Pin_5, old.Hit_Pin_6, old.Hit_Pin_7,
+    old.Hit_Pin_8, old.Hit_Pin_9, old.Hit_Pin_10, old.Date_Added, old.Last_Date_Modified, now());
 
 drop trigger if exists delete_from_Game;
 CREATE TRIGGER delete_from_Game AFTER DELETE ON Game
 FOR EACH ROW
-  insert into Game_Archive VALUES (old.Game_ID, old.Teams, old.Game_Start_Time, old.Game_End_Time, old.Winner_Team_ID, old.Title, old.Location_ID, old.Event_Type, old.Game_Finished , old.Date_Added, old.Last_Date_Modified, now());
+  insert into Game_Archive VALUES (old.Game_ID, old.Teams,
+    old.Game_Start_Time, old.Game_End_Time, old.Winner_Team_ID,
+    old.Title, old.Location_ID, old.Event_Type, old.Game_Finished ,
+    old.Date_Added, old.Last_Date_Modified, now());
 
 drop trigger if exists delete_from_PlayerStats;
 CREATE TRIGGER delete_from_PlayerStats AFTER DELETE ON Player_Stats
 FOR EACH ROW
-  insert into Player_Stats_Archive VALUES (old.Stat_ID, old.Player_ID, old.Strikes, old.Games_Played, old.Perfect_Games, old.Spares, old.Best_Score, old.Worst_Score, old.Pins_Left, old.Average_Pin_Left, old.Date_Added, old.Last_Date_Modified, old.Foul_Count, old.Pins_Hit, now());
+  insert into Player_Stats_Archive VALUES (old.Stat_ID, old.Player_ID,
+    old.Strikes, old.Games_Played, old.Perfect_Games, old.Spares,
+    old.Best_Score, old.Worst_Score, old.Pins_Left, old.Average_Pin_Left, old.Date_Added,
+    old.Last_Date_Modified, old.Foul_Count, old.Pins_Hit, now());
 
 drop trigger if exists delete_from_game_location;
 CREATE TRIGGER delete_from_game_location AFTER DELETE ON Game_Location
 FOR EACH ROW
-  insert into Game_Location_Archive VALUES (old.Game_Location_ID, old.Game_Address, old.Game_Location_Name, old.Date_Added, old.Last_Date_Modified, now());
+  insert into Game_Location_Archive VALUES (old.Game_Location_ID,
+    old.Game_Address, old.Game_Location_Name, old.Date_Added,
+    old.Last_Date_Modified, now());
 
 drop trigger if exists update_player_stats;
 create trigger update_player_stats after insert on Roll
   for each ROW begin
-  UPDATE Player_Stats, Frame
-  set Player_Stats.Pins_Hit = Player_Stats.Pins_Hit +
-                    new.Hit_Pin_1 + new.Hit_Pin_2 + new.Hit_Pin_3 + new.Hit_Pin_4 + new.Hit_Pin_5 + new.Hit_Pin_6 + new.Hit_Pin_7 + new.Hit_Pin_8
-                  + new.Hit_Pin_9 + new.Hit_Pin_10 where (new.Frame_ID = Frame.Frame_ID) and frame.Player_ID = Player_Stats.Player_ID;
+  UPDATE bowling.Player_Stats, Frame
+  set bowling.Player_Stats.Pins_Hit = bowling.Player_Stats.Pins_Hit +
+    new.Hit_Pin_1 + new.Hit_Pin_2 + new.Hit_Pin_3 + new.Hit_Pin_4 + new.Hit_Pin_5 +
+    new.Hit_Pin_6 + new.Hit_Pin_7 + new.Hit_Pin_8 + new.Hit_Pin_9 + new.Hit_Pin_10
+  where (new.Frame_ID = Frame.Frame_ID) and frame.Player_ID = Player_Stats.Player_ID;
+
   if(new.Is_Spare = 1) THEN
-    update Player_Stats, Frame SET Player_Stats.Spares = Player_Stats.Spares + 1 where (new.Frame_ID = Frame.Frame_ID) and frame.Player_ID = Player_Stats.Player_ID;
+    update Player_Stats, Frame
+    SET Player_Stats.Spares = Player_Stats.Spares + 1
+    where (new.Frame_ID = Frame.Frame_ID) and frame.Player_ID = Player_Stats.Player_ID;
   ELSEIF (new.Is_Strike = 1) THEN
-    update Player_Stats, Frame SET Player_Stats.Strikes = Player_Stats.Strikes + 1 where (new.Frame_ID = Frame.Frame_ID) and frame.Player_ID = Player_Stats.Player_ID;
+    update Player_Stats, Frame
+    SET Player_Stats.Strikes = Player_Stats.Strikes + 1
+    where (new.Frame_ID = Frame.Frame_ID) and frame.Player_ID = Player_Stats.Player_ID;
   ELSEIF (new.Is_Foul = 1) THEN
-    update Player_Stats, Frame SET Player_Stats.Foul_Count = Player_Stats.Foul_Count + 1 where (new.Frame_ID = Frame.Frame_ID) and frame.Player_ID = Player_Stats.Player_ID;
+    update Player_Stats, Frame
+    SET Player_Stats.Foul_Count = Player_Stats.Foul_Count + 1
+    where (new.Frame_ID = Frame.Frame_ID) and frame.Player_ID = Player_Stats.Player_ID;
   END IF;
 end;
 
