0cf1254b30d81d4462276ff6ed62404d6b4accb2
William <wnguyen23@gmail.com>
2016-04-25 15:37:36 -0500

    -Email is sent to team leader when a player requests to join a team

--- public_html/sendEmailToTeamLeader.php
+++ public_html/sendEmailToTeamLeader.php
@@ -3,7 +3,26 @@ include 'databaseFunctions.php';
 function createTeamLeaderArray()
 {
     $conn = connectToDatabase();
-    $sql = "SELECT First_Name, Last_Name, Player_ID from Players";
+    $sql = "SELECT Player_ID, Email from Players";
+    $result = $conn->query($sql);
+    $teamLeader = array();
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $teamLeader[$row['Player_ID']] = $row['Email'];
+        }
+    } else {
+        echo "0 results";
+    }
+    return $teamLeader;
+
+}
+?>
+
+<?php
+function nameArray()
+{
+    $conn = connectToDatabase();
+    $sql = "SELECT Player_ID, First_Name, Last_Name from Players";
     $result = $conn->query($sql);
     $teamLeader = array();
     if ($result->num_rows > 0) {
@@ -14,6 +33,7 @@ function createTeamLeaderArray()
         echo "0 results";
     }
     return $teamLeader;
+
 }
 ?>
 
@@ -37,13 +57,21 @@ function createPlayerEmailArray()
 ?>
 
 <?php
+session_start();
+function popupMessageAndRedirectBrowser($message) {
+    echo "<script type='text/javascript'>alert('$message');history.go(-1);document.location = 'joinTeam.php';</script>";
+}
     $players = createPlayerEmailArray();
-    echo 'got id' . $_POST['teamID'];
-    echo '<br> emailing...' . $players[$_POST['teamID']];
-//    include 'sendEmail.php';
+    $name = nameArray();
+
+//    echo 'got id' . $_POST['teamID'];
+//    echo '<br> emailing...' . $players[$_POST['teamID']];
+    include 'sendEmail.php';
 //    echo "<br> You have succesfully requested to join the team!";
-//    echo "<br> The team leader has received your request and will get back to you soon!";
-//    sendEmailTo("wnguyen23@yahoo.com","William","Nguyen");
+    $message = "The team leader has received your request and will get back to you soon!";
+    sendEmailTo($players[$_POST['teamID']],$name[$_SESSION['player_id']], $players[$_SESSION['player_id']]);
+    popupMessageAndRedirectBrowser($message);
+
 
 ?>
 
