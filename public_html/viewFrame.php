4dc2b9ed2ec6e67738fa805820e5f7af22ba453d
robert-vo <robertvo79@gmail.com>
2016-04-25 21:50:29 -0500

    Printed frame summary

--- public_html/viewFrame.php
+++ public_html/viewFrame.php
@@ -30,20 +30,39 @@ function getRollInformationForFrame($frameID, $frameNumber) {
 
     $result = $conn->query($sql);
 
+    $hitpin1 = 0;
+    $hitpin2 = 0;
+    $hitpin3 = 0;
+    $hitpin4 = 0;
+    $hitpin5 = 0;
+    $hitpin6 = 0;
+    $hitpin7 = 0;
+    $hitpin8 = 0;
+    $hitpin9 = 0;
+    $hitpin10 = 0;
+
     if($result->num_rows > 0) {
         $roll = 1;
         while($row = $result->fetch_assoc()) {
             echo '<br>On Frame Number ' . $frameNumber . ', and for roll number ' . $roll . ', the ' . $row['Size'] . ' ';
             $roll++;
-            echo $row['Color'] . ' ball that weighted ' . $row['Weight'] . ' pounds was used.';
 
-            echo '<br>Here is what happened to the following pins:';
+
+
+            echo $row['Color'] . ' ball that weighted ' . $row['Weight'] . ' pounds was used.';
+            $color = $row['Color'];
+            echo ' Bowling ball for reference: <svg height = 50 width = 50>';
+            echo "<circle cx=25 cy=25 r=20 stroke=black stroke-width=2 fill='$color'/>";
+            echo '</svg>';
+
+            echo '<br>Here are the new pins hit:<br>';
 
             echo '<table><tr><td>Pin 1</td><td>Pin 2</td><td>Pin 3</td><td>Pin 4</td><td>Pin 5</td><td>Pin 6</td><td>Pin 7</td><td>Pin 8</td><td>Pin 9</td><td>Pin 10</td></tr>';
 
             echo '<tr>';
             if($row['Hit_Pin_1'] == 1) {
                 printBall(1, 1);
+                $hitpin1 = 1;
             }
             else {
                 printBall(1, 0);
@@ -51,6 +70,7 @@ function getRollInformationForFrame($frameID, $frameNumber) {
 
             if($row['Hit_Pin_2'] == 1) {
                 printBall(2, 1);
+                $hitpin2 = 1;
             }
             else {
                 printBall(2, 0);
@@ -58,6 +78,7 @@ function getRollInformationForFrame($frameID, $frameNumber) {
 
             if($row['Hit_Pin_3'] == 1) {
                 printBall(3, 1);
+                $hitpin3 = 1;
             }
             else {
                 printBall(3, 0);
@@ -65,6 +86,7 @@ function getRollInformationForFrame($frameID, $frameNumber) {
 
             if($row['Hit_Pin_4'] == 1) {
                 printBall(4, 1);
+                $hitpin4 = 1;
             }
             else {
                 printBall(4, 0);
@@ -72,6 +94,7 @@ function getRollInformationForFrame($frameID, $frameNumber) {
 
             if($row['Hit_Pin_5'] == 1) {
                 printBall(5, 1);
+                $hitpin5 = 1;
             }
             else {
                 printBall(5, 0);
@@ -79,6 +102,7 @@ function getRollInformationForFrame($frameID, $frameNumber) {
 
             if($row['Hit_Pin_6'] == 1) {
                 printBall(6, 1);
+                $hitpin6 = 1;
             }
             else {
                 printBall(6, 0);
@@ -86,6 +110,7 @@ function getRollInformationForFrame($frameID, $frameNumber) {
 
             if($row['Hit_Pin_7'] == 1) {
                 printBall(7, 1);
+                $hitpin7 = 1;
             }
             else {
                 printBall(7, 0);
@@ -93,6 +118,7 @@ function getRollInformationForFrame($frameID, $frameNumber) {
 
             if($row['Hit_Pin_8'] == 1) {
                 printBall(8, 1);
+                $hitpin8 = 1;
             }
             else {
                 printBall(8, 0);
@@ -100,6 +126,7 @@ function getRollInformationForFrame($frameID, $frameNumber) {
 
             if($row['Hit_Pin_9'] == 1) {
                 printBall(9, 1);
+                $hitpin9 = 1;
             }
             else {
                 printBall(9, 0);
@@ -107,6 +134,7 @@ function getRollInformationForFrame($frameID, $frameNumber) {
 
             if($row['Hit_Pin_10'] == 1) {
                 printBall(10, 1);
+                $hitpin19 = 1;
             }
             else {
                 printBall(10, 0);
@@ -145,6 +173,81 @@ function getRollInformationForFrame($frameID, $frameNumber) {
         }
     }
 
+    echo '<br>Out of all of the frames, here are all of the pins you hit.';
+    echo '<table><tr><td>Pin 1</td><td>Pin 2</td><td>Pin 3</td><td>Pin 4</td><td>Pin 5</td><td>Pin 6</td><td>Pin 7</td><td>Pin 8</td><td>Pin 9</td><td>Pin 10</td></tr>';
+
+    echo '<tr>';
+    if($hitpin1) {
+        echo '<td>✓</td>';
+    }
+    else {
+        echo '<td></td>';
+    }
+
+    if($hitpin2) {
+        echo '<td>✓</td>';
+    }
+    else {
+        echo '<td></td>';
+    }
+
+    if($hitpin3) {
+        echo '<td>✓</td>';
+    }
+    else {
+        echo '<td></td>';
+    }
+
+    if($hitpin4) {
+        echo '<td>✓</td>';
+    }
+    else {
+        echo '<td></td>';
+    }
+
+    if($hitpin5) {
+        echo '<td>✓</td>';
+    }
+    else {
+        echo '<td></td>';
+    }
+
+    if($hitpin6) {
+        echo '<td>✓</td>';
+    }
+    else {
+        echo '<td></td>';
+    }
+
+    if($hitpin7) {
+        echo '<td>✓</td>';
+    }
+    else {
+        echo '<td></td>';
+    }
+
+    if($hitpin8) {
+        echo '<td>✓</td>';
+    }
+    else {
+        echo '<td></td>';
+    }
+
+    if($hitpin9) {
+        echo '<td>✓</td>';
+    }
+    else {
+        echo '<td></td>';
+    }
+
+    if($hitpin10) {
+        echo '<td>✓</td>';
+    }
+    else {
+        echo '<td></td>';
+    }
+
+    echo '</tr>';
 }
 
 getRollInformationForFrame($_GET['frameID'], $_GET['frameNumber']);
